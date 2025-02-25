e12b6ba272c3041f8e5c58e79b428cd42cd2ae4f
Max Campos <mcampos2a@gmail.com>
2025-02-25 13:32:37 +0100

    added port

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ let db,
     dbConnecitonString = process.env.DB_STRING,
     dbName = 'Template-Demo',
     collection 
-
+//this connects us to our database
 MongoClient.connect(dbConnecitonString)
     .then(client => {
         console.log('Connected to Database')
         db = client.db(dbName)
         collection = db.collection('template-collection')
     })
+//this set up a port to listen for the server
+//PORT = 8000
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
